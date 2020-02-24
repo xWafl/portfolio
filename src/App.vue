02d1f2fff4c4e9e10a82cfc3c920c77e4e09d200
xWafl <73sampleperson@gmail.com>
2020-02-24 17:29:59 -0500

    changed modifications to changes to make justify neater

--- src/App.vue
+++ src/App.vue
@@ -8,7 +8,7 @@
         <br><br>
         <h2 style="font-size: 40px;">Portfolio</h2>
         <div id="projectDivs">
-            <ProjectCard :backgroundUrl="petrisIcon" hoverHeader="Petris" hoverText="A web version of Tetris with a lot of modifications. It's a research project for the P vs NP problem." clickUrl="http://petris.herokuapp.com"></ProjectCard>
+            <ProjectCard :backgroundUrl="petrisIcon" hoverHeader="Petris" hoverText="A web version of Tetris with a lot of changes. It's a research project for the P vs NP problem." clickUrl="http://petris.herokuapp.com"></ProjectCard>
             <ProjectCard :backgroundUrl="waterdropIcon" hoverHeader="This site!" hoverText="This site is actually a finished front end project, and it's the project that I'm the most proud of my design for." clickUrl="/"></ProjectCard>
             <ProjectCard backgroundUrl="https://www.jing.fm/clipimg/detail/221-2218831_clip-art-stock-vector-trading-chart-stock-market.png" hoverHeader="Skymarket" hoverText="An auction house analyzer for the game Hypixel Skyblock. Granted, I didn't make the backend, but I did a lot of the front-end and data analysis." clickUrl="http://xwafl.github.io/skymarket"></ProjectCard>
         </div>
